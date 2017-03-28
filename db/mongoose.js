2f0c1fb8e113b33c24b50ddaeaeed3b9825e5bb4
quyencao <boy20195@gmail.com>
2017-03-29 01:39:57 +0700

    New COmmit

--- db/mongoose.js
+++ db/mongoose.js
@@ -8,7 +8,7 @@ if(env === 'development') {
     process.env.MONGODB_URI = 'mongodb://localhost:27017/shichidaapp';
 }
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://quyen:asdasdasdasdasdasdasfadf@ds143030.mlab.com:43030/shichidaapp');
 
 module.exports = {
     mongoose
